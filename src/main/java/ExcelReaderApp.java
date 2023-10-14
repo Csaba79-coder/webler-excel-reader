d367223737a7af17393550e2419db6df47057370
Csaba79-coder <csabavadasz79@gmail.com>
2023-10-14 15:54:20 +0200

    Initial commit

---
+++ src/main/java/ExcelReaderApp.java
@@ -0,0 +1,10 @@
+public class ExcelReaderApp {
+
+    // first step add dependency to pom.xml
+    // https://mvnrepository.com/artifact/org.apache.poi/poi/5.2.4
+    // the newest version does not work :) find a previous!!!
+
+    public static void main(String[] args) {
+
+    }
+}
